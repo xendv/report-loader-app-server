5b166e04d9d30400972ff32d791917bfbf678ddd
xendv <dvoynina.ksusha@mail.ru>
2021-06-08 01:04:15 +0300

    added getDBIndContentByOKPO

--- api.php
+++ api.php
@@ -11,38 +11,75 @@
         if (is_uploaded_file($_FILES["file"]["tmp_name"])) {
             $uploaded_file=$_FILES["file"]["tmp_name"];
             $original_file=$_FILES["file"]["name"];
-            echo "- File was posted. Path: ".$uploaded_file.PHP_EOL;
-            echo('- Original file name: '.$original_file.PHP_EOL);
-            echo("- Original file type: ".$_POST["type"].PHP_EOL);        
+            //echo "- File was posted. Path: ".$uploaded_file.PHP_EOL;
+            //echo('- Original file name: '.$original_file.PHP_EOL);
+            //echo("- Original file type: ".$_POST["type"].PHP_EOL);        
             
             $temp_file_name_with_ext = substr($_FILES['file']['tmp_name'],
             1 + strrpos($_FILES['file']['tmp_name'], "\\"));
-            echo('- Temp name + ext: '.$temp_file_name_with_ext.PHP_EOL);
+            //echo('- Temp name + ext: '.$temp_file_name_with_ext.PHP_EOL);
 
             $temp_file_name=basename($_FILES['file']['tmp_name'], ".tmp");
-            echo('- Temp name: '.$temp_file_name.PHP_EOL);
+            //echo('- Temp name: '.$temp_file_name.PHP_EOL);
 
             $new_temp_file_name=$temp_file_name.'_'.$_FILES["file"]["name"];
-            move_uploaded_file($_FILES["file"]["tmp_name"], './temp/'.$new_temp_file_name);
+            move_uploaded_file($_FILES["file"]["tmp_name"], $upload_tmp_dir.$new_temp_file_name);
 
-            $newTempFilePath='./temp/'.$new_temp_file_name;           
+            $newTempFilePath=$upload_tmp_dir.$new_temp_file_name;           
 
-            echo("- New temp file path: ".$newTempFilePath.PHP_EOL);
-            getDataFromFile($newTempFilePath);
+            //echo("- New temp file path: ".$newTempFilePath.PHP_EOL);
+            echo json_encode(getDataFromFile($newTempFilePath));
             exit;
         }else {  
-            echo 'No file was posted';
+            throw new ErrorException("No file was posted");
         }
+    }
+    elseif ($_POST["action"]=="getDBMainContent"){
+        $dataBaseManager = new dataBaseManager;
+        if ($dataBaseManager->connectToDb()){
+            $data=$dataBaseManager->getDBMainContent();
+            return $data;
+            //echo "GOT CONNECTION";
+        }
+        else alert("OOPS NO CONNECTION");
+        return false;
+
+    }
+    elseif ($_POST["action"]=="getDBIndContent"){
+        $dataBaseManager = new dataBaseManager;
+        if ($dataBaseManager->connectToDb()){
+            $data=$dataBaseManager->getDBIndContentByOKPO($_POST["okpo"]);
+            return $data;
+            //echo "GOT CONNECTION";
+        }
+        else alert("OOPS NO CONNECTION");
+        return false;
+
+    }
+    elseif ($_POST["action"]=="getDBContent"){
+        $dataBaseManager = new dataBaseManager;
+        if ($dataBaseManager->connectToDb()){
+            echo "GOT CONNECTION";
+        }
+        else alert("OOPS NO CONNECTION");
+
     }
 
     
     function getDataFromFile($file){
         $dataVisualiser = new dataVisualiser;
-        $data=$dataVisualiser->getDataFromCSVFileByPath($file);
-        //echo $file;
-        //$data=$dataVisualiser->getDataFromCSVFileByPath('./'.$tmp_name);
-        echo "Данные файла: ";
-        print_r($data);
+        if($_POST["type"]=='csv' || $_POST["type"]=='CSV')
+        {
+            $data=$dataVisualiser->getDataFromCSVFileByPath($file);
+            //echo $file;
+            //$data=$dataVisualiser->getDataFromCSVFileByPath('./'.$tmp_name);
+            //echo "Данные файла: ".PHP_EOL;
+            //echo $data;
+            return $data;
+        }
+        elseif ($_POST["type"]=='dbf' || $_POST["type"]=='DBF'){
+
+        }
     }
 	
     //uploadData($_FILES["file"]["tmp_name"]);
